7bb6e05bb914425ba3f1542a41a7d51713624dc4
Pedro Gutierrez Jr <gutierrezpedro2112@gmail.com>
2023-08-12 12:53:29 -0500

    player functionality implemented, main app functions done

---
+++ components/Icon.tsx
@@ -0,0 +1,5 @@
+import { BsPauseFill, BsPlayFill } from "react-icons/bs";
+
+const Icon = false ? BsPauseFill : BsPlayFill;
+
+export default Icon;
---
+++ components/VolumeIcon.tsx
@@ -0,0 +1,5 @@
+import { HiSpeakerXMark, HiSpeakerWave } from "react-icons/hi2";
+
+const VolumeIcon = true ? HiSpeakerXMark : HiSpeakerWave;
+
+export default VolumeIcon;
